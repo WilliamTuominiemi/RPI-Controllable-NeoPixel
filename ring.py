4e84f4c6efdca1520bec834d5a887adc3a675e85
024William <william.m.tuominiemi@gmail.com>
2021-07-31 22:36:18 +0300

    keypad code

--- ring.py
+++ ring.py
@@ -5,6 +5,7 @@ import time
 import board
 import neopixel
 
+GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 
 # Configure neopixel
@@ -33,6 +34,28 @@ GPIO.setup(clk, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(dt, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
 clkLastState = GPIO.input(clk)
+
+# Configure keypad
+
+L1 = 5
+L2 = 6
+L3 = 13
+L4 = 19
+
+C1 = 12
+C2 = 16
+C3 = 20
+C4 = 21
+
+GPIO.setup(L1, GPIO.OUT)
+GPIO.setup(L2, GPIO.OUT)
+GPIO.setup(L3, GPIO.OUT)
+GPIO.setup(L4, GPIO.OUT)
+
+GPIO.setup(C1, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(C2, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(C3, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(C4, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
 
 def wheel(pos):
@@ -64,8 +87,27 @@ def rainbow_cycle(wait):
         time.sleep(wait)
 
 
+def readLine(line, characters):
+    GPIO.output(line, GPIO.HIGH)
+    if(GPIO.input(C1) == 1):
+        print(characters[0])
+    if(GPIO.input(C2) == 1):
+        print(characters[1])
+    if(GPIO.input(C3) == 1):
+        print(characters[2])
+    if(GPIO.input(C4) == 1):
+        print(characters[3])
+    GPIO.output(line, GPIO.LOW)
+
+
 try:
     while True:
+        # Keypad
+        readLine(L1, ["1", "2", "3", "A"])
+        readLine(L2, ["4", "5", "6", "B"])
+        readLine(L3, ["7", "8", "9", "C"])
+        readLine(L4, ["*", "0", "#", "D"])
+
         clkState = GPIO.input(clk)
         dtState = GPIO.input(dt)
         if clkState != clkLastState:
